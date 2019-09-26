f949d3ead96199a795e2162bb6c15c220121f3d0
tonky0110 <tonky0110@gmail.com>
2019-09-26 22:57:17 +0900

    reset setting prismagram

---
+++ src/server.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+import { GraphQLServer } from 'graphql-yoga';
+import logger from 'morgan';
+import schema from './schema';
+
+const PORT = process.env.PORT || 4000;
+
+const server = new GraphQLServer({ schema });
+server.express.use(logger('dev'));
+server.start({ port: PORT }, () => console.log(`✅ Server is running on localhost://${PORT}`));
