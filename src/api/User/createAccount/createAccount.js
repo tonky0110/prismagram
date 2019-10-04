218cbdaaa00020d2a982d3c2526d8666fe90b13e
tonky0110 <tonky0110@gmail.com>
2019-10-04 23:04:32 +0900

    5.8 add check option(email)

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,9 +4,11 @@ export default {
 	Mutation: {
 		createAccount: async (_, args) => {
 			const { username, email, firstName = '', lastName = '', bio = '' } = args;
-			const exists = await prisma.$exists.user({ username });
+			const exists = await prisma.$exists.user({
+				OR: [ { username }, { email } ]
+			});
 			if (exists) {
-				throw Error(`This username(${username}) is already taken.`);
+				throw Error(`This username(${username}) / email(${email}) is already taken.`);
 			}
 			try {
 				await prisma.createUser({
