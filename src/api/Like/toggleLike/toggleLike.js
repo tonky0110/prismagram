65af76bf4959ad1bd3920cd615dd0527acf59d36
tonky0110 <tonky0110@gmail.com>
2019-10-09 13:18:20 +0900

    7.4 fix toggleLike on Post prisma.deleteManyLikes

--- src/api/Like/toggleLike/toggleLike.js
+++ src/api/Like/toggleLike/toggleLike.js
@@ -7,13 +7,10 @@ export default {
 			const { user } = request;
 			const { postId } = args;
 			const filterOptions = { AND: [ { user: { id: user.id } }, { post: { id: postId } } ] };
-			console.log('1) filterOptions: ', filterOptions);
 			try {
 				const existingLike = await prisma.$exists.like(filterOptions);
-				console.log('2) check existingLike: ', existingLike);
 				if (existingLike) {
 					const like = await prisma.deleteManyLikes(filterOptions);
-					console.log('3) like: ', like);
 				} else {
 					await prisma.createLike({
 						user: {
@@ -30,7 +27,6 @@ export default {
 				}
 				return true;
 			} catch (error) {
-				console.log('4) exception catch: ', error);
 				return false;
 			}
 		}
