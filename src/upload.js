2899009acfa871ea3c45a20cc8d63fa022625688
tonky0110 <tonky0110@gmail.com>
2019-12-13 22:44:16 +0900

    18.8 send file and res.json({path})

--- src/upload.js
+++ src/upload.js
@@ -4,7 +4,7 @@ const upload = multer({ dest: 'upload/' });
 export const uploadMiddleware = upload.single('file');
 
 export const uploadController = (req, res) => {
-	const { file } = req;
-	console.log('file: ', file);
-	req.end();
+	const { file: { path } } = req;
+	console.log('path: ', path);
+	res.json({ path });
 };
